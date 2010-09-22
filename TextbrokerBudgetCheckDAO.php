2bcb545c2148ce9b8b9df1c2dfdd2708b8b42f33
Fabio Bacigalupo <info1@open-haus.de>
2010-09-22 09:34:49 +0200

    Added header and more documentation

--- TextbrokerBudgetCheckDAO.php
+++ TextbrokerBudgetCheckDAO.php
@@ -1,16 +1,56 @@
 <?php
+// +---------------------------------------------------------------------------+
+// | Copyright (c) 2010, Fabio Bacigalupo                                      |
+// | All rights reserved.                                                      |
+// |                                                                           |
+// | Redistribution and use in source and binary forms, with or without        |
+// | modification, are permitted provided that the following conditions        |
+// | are met:                                                                  |
+// |                                                                           |
+// | o Redistributions of source code must retain the above copyright          |
+// |   notice, this list of conditions and the following disclaimer.           |
+// | o Redistributions in binary form must reproduce the above copyright       |
+// |   notice, this list of conditions and the following disclaimer in the     |
+// |   documentation and/or other materials provided with the distribution.    |
+// | o The names of the authors may not be used to endorse or promote          |
+// |   products derived from this software without specific prior written      |
+// |   permission.                                                             |
+// |                                                                           |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       |
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT         |
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR     |
+// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      |
+// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,     |
+// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT          |
+// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     |
+// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY     |
+// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT       |
+// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE     |
+// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.      |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+// | textbroker-PHP5-Client 0.1                                                |
+// +---------------------------------------------------------------------------+
+// | TextbrokerBudgetCheckDAO.php                                              |
+// +---------------------------------------------------------------------------+
+// | Authors: Fabio Bacigalupo <info1@open-haus.de>                            |
+// +---------------------------------------------------------------------------+
+
 require_once(dirname(__FILE__) . '/TextbrokerDAO.php');
 
 /**
  * BudgetCheckService
  *
  * Budgetinformationen abfragen
+ *
+ * @package textbroker-PHP5-Client
+ * @author Fabio Bacigalupo <info1@open-haus.de>
  */
 class TextbrokerBudgetCheckDAO extends TextbrokerDAO {
 
-    function __construct($budgetKey = null, $budgetId = null) {
+    function __construct($budgetKey = null, $budgetId = null, $password = null) {
 
-        parent::__construct($budgetKey, $budgetId);
+        parent::__construct($budgetKey, $budgetId, $password);
         $this->setOptions(array(
             'location'      => 'https://api.textbroker.de/Budget/budgetCheckService.php',
             'uri'           => self::BUDGET_URI,
